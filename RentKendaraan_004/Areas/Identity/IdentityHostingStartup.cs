08911a8e5a854ebd6404043b5a5866ce79a20c42
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2021-01-16 01:46:10 +0700

    Add project files.

---
+++ RentKendaraan_004/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentKendaraan_004.Data;
+
+[assembly: HostingStartup(typeof(RentKendaraan_004.Areas.Identity.IdentityHostingStartup))]
+namespace RentKendaraan_004.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
